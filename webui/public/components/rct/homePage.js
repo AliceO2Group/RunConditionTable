56e81ad5063dbc7dc2357f06815cb9dab2c8985b
Ehevi <aga.cyra@wp.pl>
2021-07-24 18:14:27 +0200

    fixed classes

--- webui/public/components/rct/homePage.js
+++ webui/public/components/rct/homePage.js
@@ -2,13 +2,9 @@ import { h } from '/js/src/index.js';
 import RCTTableView from './tableView.js';
 
 export default function RCTHomepage(model) {
-    const cssClass1 = '.flex-column.items-center.justify-center';
-    const cssClass2 = 'h1.primary.justify_center';
-    const cssClass3 = 'div.flex-row.items-end';
-    
-    return h(cssClass1,
-        h(cssClass2, [
-            h(cssClass3, 'RCT Homepage'),
+    return h('.flex-column.items-center.justify-center',
+        h('.bg-gray-lighter.br3.p4', [
+            h('h1.primary.justify_center', 'RCT Homepage'),
             h('div.flex-row.items-end', []),
             RCTTableView(model)
         ])
