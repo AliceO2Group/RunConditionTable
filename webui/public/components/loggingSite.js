dcc8def7925005f12623a0699ae0de4e7bb6691d
Ehevi <aga.cyra@wp.pl>
2021-07-26 12:41:48 +0200

    common components :ant:

--- webui/public/components/loggingSite.js
+++ webui/public/components/loggingSite.js
@@ -1,38 +1,38 @@
 import { h } from '/js/src/index.js';
 import sessionService from '/js/src/sessionService.js';
+import inputForm from './common/inputForm.js';
+import button from './common/button.js';
 
 export default function loggingSite(model) {
+    const handleLogin = async e => {
+        const password = document.getElementById('password').value;
+        const username = document.getElementById('username').value;
+        const dbnameEl = document.getElementById('dbname');
+        const dbname = dbnameEl.value === '' ? dbnameEl.placeholder : dbnameEl.value;
+
+        const p = await model.login(username, password, dbname);
+        console.log("Im. after logging,; sessionService: ");
+        console.log(sessionService);
+    }
+
+    const handleRegistration = e => {
+        alert("Registration has not been implemented yet :(");
+    }
+    
     return h('div.flex-column.items-center.justify-center',
         h('div.flex-column.bg-gray-lighter.br3.p4', [
             h('h1.primary', 'TEST APP'), ' ',
-            h('form', [
-                h('label', {for: "dbname"}, ""),
-                h('input', {id: "dbname", type: 'input', placeholder: "rct-db"}, '')
-            ]),
-            h('form', [
-                h('label', {for: "username"}, ""),
-                h('input', {id: "username", type: 'input', placeholder: "username"}, '')
-            ]), ' ',
-            h('form', [
-                h('label', {for: "password"}, ""),
-                h('input', {id: "password", type: 'password', placeholder: "password"}, '')
-            ]),
+            
+            h('form-group', 
+                inputForm("dbname", "dbname", "rct-db"),
+                inputForm("username", "username", "username"),
+                inputForm("password", "password", "password", true),
+            ),
 
             h('.flex-wrap.justify-center',
                 h('div', [
-                    h('.pv2', h('button.btn.justify-center.items-center', {
-                        onclick: async e => {
-                            const password = document.getElementById('password').value;
-                            const username = document.getElementById('username').value;
-                            const dbnameEl = document.getElementById('dbname');
-                            const dbname = dbnameEl.value === '' ? dbnameEl.placeholder : dbnameEl.value;
-
-                            const p = await model.login(username, password, dbname);
-                            console.log("Im. after logging,; sessionService: ");
-                            console.log(sessionService);
-                        }
-                    }, "login")),
-                    h('.pv2', h('button.btn', {onclick: e => alert("(window.sessionService)TODO, now you are logged as: " + window.sesService)}, "register"))
+                    h('.pv2', button("login", handleLogin)),
+                    h('.pv2', button("register", handleRegistration))
                 ])
             )
         ])
