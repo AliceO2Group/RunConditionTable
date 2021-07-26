1a403bb1e15d1cc1025ad518f82eb69db79b850e
Ehevi <aga.cyra@wp.pl>
2021-07-26 13:38:06 +0200

    table component

--- webui/public/components/loggingSite.js
+++ webui/public/components/loggingSite.js
@@ -2,39 +2,38 @@ import { h } from '/js/src/index.js';
 import sessionService from '/js/src/sessionService.js';
 import inputForm from './common/inputForm.js';
 import button from './common/button.js';
+import container from './common/container.js';
+
+function handleRegistration() {
+    alert("Registration has not been implemented yet :(");
+}
+
+async function handleLogin(model) {
+    const password = document.getElementById('password').value;
+    const username = document.getElementById('username').value;
+    const dbnameEl = document.getElementById('dbname');
+    const dbname = dbnameEl.value === '' ? dbnameEl.placeholder : dbnameEl.value;
+
+    const p = await model.login(username, password, dbname);
+    console.log("Logged in successfully!");
+    console.log(`sessionService: ${sessionService}`);
+}
 
 export default function loggingSite(model) {
-    const handleLogin = async e => {
-        const password = document.getElementById('password').value;
-        const username = document.getElementById('username').value;
-        const dbnameEl = document.getElementById('dbname');
-        const dbname = dbnameEl.value === '' ? dbnameEl.placeholder : dbnameEl.value;
-
-        const p = await model.login(username, password, dbname);
-        console.log("Im. after logging,; sessionService: ");
-        console.log(sessionService);
-    }
-
-    const handleRegistration = e => {
-        alert("Registration has not been implemented yet :(");
-    }
+    const loginButton = button("login", () => handleLogin(model));
+    const registerButton = button("register", handleRegistration);
+    const title = h('h1.primary', 'TEST APP');
     
     return h('div.flex-column.items-center.justify-center',
         h('div.flex-column.bg-gray-lighter.br3.p4', [
-            h('h1.primary', 'TEST APP'), ' ',
+            title,
             
-            h('form-group', 
+            container(
                 inputForm("dbname", "dbname", "rct-db"),
                 inputForm("username", "username", "username"),
-                inputForm("password", "password", "password", true),
-            ),
+                inputForm("password", "password", "password", true),),
 
-            h('.flex-wrap.justify-center',
-                h('div', [
-                    h('.pv2', button("login", handleLogin)),
-                    h('.pv2', button("register", handleRegistration))
-                ])
-            )
+            container(loginButton, registerButton),
         ])
     )
 }
