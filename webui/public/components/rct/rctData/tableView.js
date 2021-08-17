2a092dbd826d1c31b5d591e26ceef8d92fd475e3
xsalonx <lukasz.dubiel3@gmail.com>
2021-08-18 00:10:43 +0200

    move some functionality from tableView to rctDataView

--- webui/public/components/rct/rctData/tableView.js
+++ webui/public/components/rct/rctData/tableView.js
@@ -1,60 +1,46 @@
-import { h, switchCase } from '/js/src/index.js';
+import {h, switchCase} from '/js/src/index.js';
 import button from '../../common/button.js';
 import tableHeader from './table/header.js';
 import row from './table/row.js';
-import container from "../../common/container.js";
-
 import pagesCellsButtons from "./pagesCellsButtons.js";
-import spinner from "../../spinner.js";
-import filter from "./table/filter.js";
 
 
 export default function tableView(model) {
+
     const params = model.router.params;
-    var data =  model.fetchedData[params.page][params.index];
+    const data = model.fetchedData[params.page][params.index];
 
     const cellsButtons = pagesCellsButtons[params.page];
 
-    if (! data) {
-        console.error('tableView error; data===null');
-        data = model.reqForData();
-    }
-    if (data.fetched) {
-        const fields = data.fields;
-        const visibleFields = fields.filter(f => f.marked);
+    const fields = data.fields;
+    const visibleFields = fields.filter(f => f.marked);
 
-        return h('div.p3',[
-            // fieldsVisibilityControl(model, data, fields),
-            button(model, 'reload data', () => data.fetch(), 'reload-btn'), ' ', // TODO move up
-            h('table.table', {id: 'data-table-' + data.url}, [
+    return h('div.p3', [
+        // fieldsVisibilityControl(model, data, fields),
+        button(model, 'reload data', () => data.fetch(), 'reload-btn'), ' ', // TODO move up
+        h('table.table', {id: 'data-table-' + data.url}, [
 
-                    h('caption', data.name),
-                    tableHeader(visibleFields, data, () => data.changeRecordsVisibility(data)),
-                    tableBody(model, visibleFields, data, cellsButtons)
+            // h('caption', data.name),
+            tableHeader(visibleFields, data, () => data.changeRecordsVisibility(data)),
+            tableBody(model, visibleFields, data, cellsButtons)
 
-                ])
-            ])
-    } else {
-        return h('.item-center.justify-center',
-            [button(model, 'reload data', () => data.fetch(), 'reload-btn'),
-            spinner()]
-        );
-    }
+        ])
+    ])
+
 }
 
 
 function fieldsVisibilityControl(mode, data, fields) {
-    return h('.p3', fields.map(f => h('label', [
-                                        h('input.form-check-input.p3', {
-                                            onclick: () => data.changeItemStatus(f),
-                                            checked: f.marked,
-                                            type: 'checkbox'
-                                        }),
-                                        f.name,
-                                    ])
+    return h('.flex-row.p3', fields.map(f => h('label', [
+            h('input.form-check-input.p3', {
+                onclick: () => data.changeItemStatus(f),
+                checked: f.marked,
+                type: 'checkbox'
+            }),
+            f.name,
+        ])
     ))
 }
-
 
 
 function tableBody(model, visibleFields, data, cellsButtons) {
