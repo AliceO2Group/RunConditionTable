8a3ce6c0ed5296ec84bc95569efb37cb66f86d37
xsalonx <lukasz.dubiel3@gmail.com>
2021-08-19 15:22:17 +0200

    added additional controllers for site changing

--- rct-demo/app/public/components/rct/rctData/tableView.js
+++ rct-demo/app/public/components/rct/rctData/tableView.js
@@ -41,10 +41,11 @@ function siteController(model, data) {
         return button(model, site, () => data.changeSite(site), '', url.pathname + url.search, '', '.m1', true);
     })
     const sitesNumber = Math.ceil(data.totalRecordsNumber / data.rowsOnSite);
-    let currentSite = Object.fromEntries(data.url.searchParams.entries())['site'] - 1;
+    const currentSite = Number(Object.fromEntries(data.url.searchParams.entries())['site']);
+    const currentSiteIdx = currentSite - 1;
 
-    const middleButtonsR = range(Math.max(0, currentSite - visibleNeighbourButtonsRange),
-                                 Math.min(sitesNumber, currentSite + visibleNeighbourButtonsRange + 1));
+    const middleButtonsR = range(Math.max(0, currentSiteIdx - visibleNeighbourButtonsRange),
+                                 Math.min(sitesNumber, currentSiteIdx + visibleNeighbourButtonsRange + 1));
 
     const leftButtonsR = range(0,
                                Math.min(middleButtonsR[0], Math.floor((maxVisibleButtons - (2 * visibleNeighbourButtonsRange + 1)) / 2)));
@@ -55,13 +56,22 @@ function siteController(model, data) {
     const leftThreeDotsPresent = !(leftButtonsR[leftButtonsR.length - 1] === middleButtonsR[0] - 1 || leftButtonsR.length === 0);
     const rightThreeDotsPresent = !(rightButtonsR[0] === middleButtonsR[middleButtonsR.length - 1] + 1 || rightButtonsR.length === 0);
 
-    console.log(sitesNumber, leftButtonsR, middleButtonsR, rightButtonsR);
+    // TODO add tooltips
+    const siteChangingController = (onclickF, label, toggleTitle='') => h('a.site-changing-controller', {onclick: onclickF}, label);
+
     return h('.flex-row', [
+        'site:',
+        currentSite > 1 ? siteChangingController(() => data.changeSite(1), '<<') : '',
+        currentSite > 3 ? siteChangingController(() => data.changeSite(Math.floor(currentSite / 2)), '|') : '',
+        currentSite > 1 ? siteChangingController(() => data.changeSite(currentSite - 1), '<'): '',
         mapArrayToButtons(leftButtonsR),
         leftThreeDotsPresent ? '...' : '',
         mapArrayToButtons(middleButtonsR),
         rightThreeDotsPresent ? '...' : '',
         mapArrayToButtons(rightButtonsR),
+        currentSite < sitesNumber ? siteChangingController(() => data.changeSite(currentSite + 1), '>') : '',
+        currentSite < sitesNumber - 2 ? siteChangingController(() => data.changeSite(currentSite + Math.floor((sitesNumber - currentSite) / 2)), '|') : '',
+        currentSite < sitesNumber ? siteChangingController(() => data.changeSite(sitesNumber), '>>'): '',
     ]);
 
 }
