e43fb906b81ccf8d6161ed9bbec6a8ccc5f447eb
xsalonx <lukasz.dubiel3@gmail.com>
2021-12-08 00:36:36 +0100

    installation and removing service works

---
+++ rct-install.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# $1 :=: hostname
+# $2 :=: port
+
+UNIT_NAME="run-condition-table"
+
+REQUIRED_COMMANDS=(node npm)
+
+for comm in "${REQUIRED_COMMANDS[@]}"; do
+  echo $comm
+  if ! command -v node &> /dev/null; then
+    echo "could not find command "$comm
+    exit
+  fi
+done
+
+
+SERVICE_SYSTEMD=service-systemd
+if ! command -v $SERVICE_SYSTEMD &> /dev/null; then
+    echo "could not find command "$SERVICE_SYSTEMD
+    echo "permission for installation of npm package <service-systemd> [y/[n]]"
+    read permission
+    if [ "$permission" = "y" ]; then
+      npm i -g $SERVICE_SYSTEMD
+    else
+      echo "installation can not be performed"
+      exit
+    fi
+fi
+
+
+RCT_ETC_DIRECTORY=/etc/run-condition-table/
+rm -rf RCT_ETC_DIRECTORY
+mkdir $RCT_ETC_DIRECTORY
+RCT_CONFIG_FILE=$RCT_ETC_DIRECTORY"rct.config"
+
+INSTALLATION_DIRECTORY=/opt/RunConditionTable/
+rm -rf $INSTALLATION_DIRECTORY
+mkdir $INSTALLATION_DIRECTORY
+cp -r . $INSTALLATION_DIRECTORY
+
+APP_CONFIG_FILE="$INSTALLATION_DIRECTORY/app/config.js"
+for ((i=2; i<=$#; i++)); do
+  next_i=$(($i+1))
+  next_arg=${!next_i}
+  if [ "${!i}" = "--port" ] && [[ "$next_arg" =~ ^[0-9]+$ ]]; then  ## TODO informing about errors
+    sed -i -E "s/port *: *[0-9]+ *,/port: $next_arg,/" $APP_CONFIG_FILE
+  fi
+  if [ "${!i}" = "--hostname" ] && [[ -n "${!next_i}" ]]; then
+      sed -i -E "s/hostname *: *.+ *,/hostname: $next_arg,/" $APP_CONFIG_FILE
+  fi
+done
+
+
+cd $INSTALLATION_DIRECTORY/app && npm install ; cd -
+
+service-systemd --add --service $UNIT_NAME --cwd $INSTALLATION_DIRECTORY/app --app $INSTALLATION_DIRECTORY/app/index.js
+
+echo "UnitName=$UNIT_NAME" > $RCT_CONFIG_FILE
+chmod -w $RCT_CONFIG_FILE;
+
+systemctl daemon-reload
+systemctl stop $UNIT_NAME
+systemctl start $UNIT_NAME
+systemctl status $UNIT_NAME
