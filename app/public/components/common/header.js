63da5e0da95b91a22daf4a3025f1777014b6c468
Ehevi <aga.cyra@wp.pl>
2021-12-08 04:09:19 +0100

    header specific title :herb:

--- app/public/components/common/header.js
+++ app/public/components/common/header.js
@@ -3,7 +3,7 @@ import { h, iconHome, iconPerson } from '/js/src/index.js';
 
 export default function header(model) {
     return h('.flex-row.p2', [
-        h('.flex-grow', [
+        h('.w-50', [
             h('button.btn', iconHome()),
             ' ',
             h('button.btn', iconPerson()),
@@ -12,6 +12,21 @@ export default function header(model) {
             // h('button.btn', iconPerson(), {onclick: () => model.logout()}),
             ' ',
             h('span.f4.gray', 'Run Condition Table'),
-        ])
+        ]),
+        h('.w-50', headerSpecific(model)),
     ]);
+}
+
+const headerSpecific = (model) => {
+    switch (model.router.params.section) {
+        case 'main': return title('Periods');
+        case 'runsPerPeriod': return title('Runs per Period');
+        case 'mc': return title('Monte Carlo');
+        case 'flags': return title('Flags');
+        default: return null;
+    }
+  };
+
+const title = (text) => {
+    return h('b.f4', text);
 }
