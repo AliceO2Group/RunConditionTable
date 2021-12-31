077a4acf56e01ee1ba9ce96402205e8af4af1aad
xsalonx <lukasz.dubiel3@gmail.com>
2021-12-31 02:44:47 +0100

    code refactoring

--- rct-install.sh
+++ rct-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# arguments syntax [--hostname <hostname>] | [--port <port>]
+# arguments syntax [--hostname <hostname>] [--port <port>]
 # as in default config.js:
 #     default hostname : localhost
 #     default port : 8080
@@ -11,33 +11,33 @@ REQUIRED_COMMANDS=(node npm)
 
 for comm in "${REQUIRED_COMMANDS[@]}"; do
   echo $comm
-  if ! command -v node &> /dev/null; then
+  if ! command -v $comm &> /dev/null; then
     echo "could not find command "$comm
     exit
   fi
 done
 
 
-SERVICE_SYSTEMD=service-systemd
+SERVICE_SYSTEMD="service-systemd"
 if ! command -v $SERVICE_SYSTEMD &> /dev/null; then
     echo "could not find command "$SERVICE_SYSTEMD
     echo "permission for installation of npm package <service-systemd> [y/[n]]"
     read permission
     if [ "$permission" = "y" ]; then
-      npm i -g $SERVICE_SYSTEMD
+      npm i -g $SERVICE_SYSTEMD || echo "Error occurred during installation of <$SERVICE_SYSTEMD>"; exit 1;
     else
-      echo "installation can not be performed"
+      echo "permission for installation of <$SERVICE_SYSTEMD> denied"
       exit
     fi
 fi
 
 
-RCT_ETC_DIRECTORY=/etc/run-condition-table/
+RCT_ETC_DIRECTORY=/etc/run-condition-table
 rm -rf $RCT_ETC_DIRECTORY
 mkdir $RCT_ETC_DIRECTORY
-RCT_CONFIG_FILE=$RCT_ETC_DIRECTORY"rct.config"
+RCT_CONFIG_FILE="$RCT_ETC_DIRECTORY/rct.config"
 
-INSTALLATION_DIRECTORY=/opt/RunConditionTable/
+INSTALLATION_DIRECTORY=/opt/RunConditionTable
 rm -rf $INSTALLATION_DIRECTORY
 mkdir $INSTALLATION_DIRECTORY
 cp -r . $INSTALLATION_DIRECTORY
@@ -55,9 +55,9 @@ for ((i=1; i<=$#; i++)); do
 done
 
 
-cd $INSTALLATION_DIRECTORY/app && npm install ; cd -
+cd "$INSTALLATION_DIRECTORY/app" && npm install ; cd -
 
-service-systemd --add --service $UNIT_NAME --cwd $INSTALLATION_DIRECTORY/app --app $INSTALLATION_DIRECTORY/app/index.js
+service-systemd --add --service $UNIT_NAME --cwd "$INSTALLATION_DIRECTORY/app" --app "$INSTALLATION_DIRECTORY/app/index.js"
 
 echo "UnitName=$UNIT_NAME" > $RCT_CONFIG_FILE
 chmod -w $RCT_CONFIG_FILE;
