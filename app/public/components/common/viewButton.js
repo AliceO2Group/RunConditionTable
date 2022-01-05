3c29cdfcfbf3a2fee8dad3c4c1625517279d069c
Ehevi <aga.cyra@wp.pl>
2022-01-05 14:56:39 +0100

    selecting pages buttons

--- app/public/components/common/viewButton.js
+++ app/public/components/common/viewButton.js
@@ -3,6 +3,12 @@ import { h } from '/js/src/index.js';
 export default function viewButton(model, label, onclickAction, id = '', href = undefined, adClasses='', margin='.m1', forceButton=false) {
     let buttonType = '.btn' + adClasses + margin;
 
+    if (model.router && href !== undefined) {
+        const url = model.router.getUrl();
+        if (href === (url.pathname + url.search) || (href.hasOwnProperty('href') && (href.href === url.href)))
+            buttonType += 'selected';
+    }
+
     if (typeof(href) !== "undefined" && !forceButton)
         return h('a' + buttonType, {id: id, onclick: onclickAction, href: href}, label);
     return h('button' + buttonType, {id: id, onclick: onclickAction}, label);
