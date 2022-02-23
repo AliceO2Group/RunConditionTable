4de97a1466c135ba4135d56d6c26150f22340067
Ehevi <aga.cyra@wp.pl>
2022-02-23 09:30:49 +0100

    reading input values :books:

--- app/public/components/rct/rctData/table/filter.js
+++ app/public/components/rct/rctData/table/filter.js
@@ -18,17 +18,33 @@ import { h } from '/js/src/index.js';
 import {getPathElems} from "../../../../utils/utils.js";
 import {defaultIndex} from "../../../../utils/defaults.js";
 
+let inputFieldIds = [];
+
 export default function filter(model) {
     const pathIdent = getPathElems(model.router.getUrl().pathname);
     const data = model.fetchedData[pathIdent[0]][defaultIndex(pathIdent[1])].payload;
     const fields = data.fields;
+    // let inputFieldIds;
 
     return h('table.table-filters', [
         h('tbody', [
             labelsRow(model, fields),
             matchOrLowerBoundsInputs(fields),
             excludeOrUpperBoundsInputs(fields),
-        ])
+        ]),
+        h('button.btn', {
+            onclick: () => {
+                // model.router.go(/* */);
+            }
+        }, 'Submit'),
+        h('button.btn', {
+            onclick: () => {
+                inputFieldIds.forEach(inputFieldId => {
+                        console.log(inputFieldId);
+                        document.getElementById(inputFieldId).value=''
+                    });
+            }
+        }, 'Clear filters')
     ]);
 }
 
@@ -63,8 +79,35 @@ const createClickableLabel = (model, field) => h('td', h('button.btn.filterLabel
     className: field.marked ? 'active' : ''
 }, field.name));
 
-const createInputField = (field, command) => h('td', h('input.form-control', {
+const createInputField = (field, command) => {
+    const fieldId = `${field.name}-${command}`;
+    inputFieldIds.push(fieldId);
+
+    return h('td', h('input.form-control', {
     style: 'width:120px',
     type: 'text',
     placeholder: '',
-}));
+    id: fieldId,
+    // onchange: log,
+    onblur: log,
+}))};
+
+function log(e) {
+    console.log(`${e.target.value}`);
+
+    console.log(inputFieldIds);
+
+
+    /*
+    const input = document.querySelector(inputFieldId);
+    const log = document.getElementById('values');
+    */
+    
+    // input.addEventListener('input', updateValue);
+
+    /*
+    function updateValue(e) {
+        log.textContent = e.target.value;
+    }
+    */
+}
