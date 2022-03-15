e1ca5c1d55cedbc5c1629b39b1ac4f666f47ce21
Ehevi <aga.cyra@wp.pl>
2022-03-15 18:19:40 +0100

    using sql with clauses

--- app/lib/ReqParser.js
+++ app/lib/ReqParser.js
@@ -24,13 +24,66 @@ const pagesNames = config.public.pagesNames;
 const DRP = config.public.dataReqParams;
 const filteringParams = config.public.filteringParams;
 
-const p = 'periods'; // query.page
-const r = 'runs';
-const dp = 'data_passes';
-const dpr = 'data_passes_runs';
-const sp = 'simulation_passes';
-const spr = 'simulation_passes_runs';
-const qcf = 'quality_control_flags';
+const period_view = `WITH period_view AS (
+    SELECT name, year, (
+                        SELECT beam_type
+                        FROM beams_dictionary
+                        AS bd where bd.id = p.beam_type_id
+                    ) AS beam,
+                    (
+                        SELECT string_agg(distinct energy_per_beam, ', ')
+                        FROM runs as r
+                        WHERE r.period_id = p.id
+                        GROUP BY r.period_id
+                    ) AS energy
+    FROM periods AS p
+    )`;
+
+const runs_per_period_view = (query) => `WITH runs_per_period_view AS (
+    SELECT *
+    FROM runs
+    WHERE period_id = (
+                        SELECT id 
+                        FROM periods 
+                        WHERE periods.name = '${query.index}'
+                        )
+)`;
+
+const data_passes_view = (query) => `WITH data_passes_view AS (
+    SELECT *
+    FROM data_passes AS dp
+    WHERE exists (
+                    SELECT *
+                    FROM runs AS r
+                    INNER JOIN
+                    data_passes_runs AS dpr
+                        ON r.id = dpr.run_id
+                    INNER JOIN data_passes AS dp
+                        ON dp.id = dpr.production_id
+                    WHERE r.period_id = (
+                                        SELECT id 
+                                        FROM periods AS p 
+                                        WHERE p.name = \'${query.index}\')
+                                        )
+)`;
+
+const mc_view = (query) => `WITH mc_view AS (
+    SELECT * 
+    FROM simulation_passes AS sp 
+    WHERE exists (
+                    SELECT * 
+                    FROM runs AS r 
+                    INNER JOIN simulation_passes_runs AS spr 
+                        ON r.id = spr.run_id 
+                    INNER JOIN simulation_passes AS sp 
+                        ON sp.id = spr.simulation_pass_id 
+                    WHERE r.period_id = (
+                                        SELECT id 
+                                        FROM periods as p 
+                                        WHERE p.name = \'${query.index}\'
+                                        )
+                    ) 
+)`;
 
 class ReqParser {
 
@@ -79,65 +132,28 @@ class ReqParser {
 
         switch (query.page) {
             case pagesNames.periods:
-                return `SELECT name, year, (
-                                            SELECT beam_type
-                                            FROM beams_dictionary
-                                            AS bd where bd.id = p.beam_type_id
-                                            ) AS beam,
-                                        (
-                                            SELECT string_agg(distinct energy_per_beam, ', ')
-                                            FROM runs as r
-                                            WHERE r.period_id = p.id
-                                            GROUP BY r.period_id
-                                        ) AS energy
-                        FROM periods AS p
+                return `${period_view}
+                        SELECT name, year, beam, energy
+                        FROM period_view
                         ${filteringPart(query)}
                         ${dataSubsetQueryPart(query)};`;
 
             case pagesNames.runsPerPeriod:
-                return `SELECT *
-                        FROM runs
-                        WHERE period_id = (
-                                            SELECT id 
-                                            FROM periods 
-                                            WHERE periods.name = '${query.index}'
-                                          )
+                return `${runs_per_period_view(query)}
+                        SELECT *
+                        FROM runs_per_period_view
                          ${dataSubsetQueryPart(query)};`;
 
             case pagesNames.dataPasses:
-                return `SELECT *
-                        FROM data_passes AS dp
-                        WHERE exists (
-                                    SELECT *
-                                    FROM runs AS r
-                                    INNER JOIN
-                                    data_passes_runs AS dpr
-                                        ON r.id = dpr.run_id
-                                    INNER JOIN data_passes AS dp
-                                        ON dp.id = dpr.production_id
-                                    WHERE r.period_id = (
-                                                        SELECT id 
-                                                        FROM periods AS p 
-                                                        WHERE p.name = \'${query.index}\')
-                                                        )
-                                    ${dataSubsetQueryPart(query)};`;
+                return `${data_passes_view(query)}
+                        SELECT *
+                        FROM data_passes_view
+                        ${dataSubsetQueryPart(query)};`;
 
             case pagesNames.mc:
-                return `SELECT * 
-                        FROM simulation_passes AS sp 
-                        WHERE exists (
-                                    SELECT * 
-                                    FROM runs AS r 
-                                    INNER JOIN simulation_passes_runs AS spr 
-                                        ON r.id = spr.run_id 
-                                    INNER JOIN simulation_passes AS sp 
-                                        ON sp.id = spr.simulation_pass_id 
-                                    WHERE r.period_id = (
-                                                        SELECT id 
-                                                        FROM periods as p 
-                                                        WHERE p.name = \'${query.index}\'
-                                                        )
-                                    ) 
+                return `${mc_view(query)}
+                        SELECT * 
+                        FROM mc_view
                         ${dataSubsetQueryPart(query)};`;
 
             // case 'flags':
