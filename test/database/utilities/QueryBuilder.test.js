e3a9b7c782e50bdafa536d6e6a85e19969d3f778
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 <aga.cyra@wp.pl>
2022-06-11 17:11:55 +0200

    QueryBuilder test :ocean:

--- test/database/utilities/QueryBuilder.test.js
+++ test/database/utilities/QueryBuilder.test.js
@@ -1,4 +1,5 @@
 const QueryBuilder = require('../../../app/lib/database/QueryBuilder');
+const views = require('../../../app/lib/database/viewsDefinitions');
 const expect = require('chai').expect;
 
 module.exports = () => {
@@ -7,9 +8,32 @@ module.exports = () => {
             it('build query', async () => {
                 const queryBuilder = new QueryBuilder();
 
-                const mockData = [];
-                const result = queryBuilder.build(mockData);
+                const params = {};
+                const result = queryBuilder.build(params);
                 expect(result).to.not.be.null;
+            });
+
+            it('return SELECT NOW() as a default', async () => {
+                const queryBuilder = new QueryBuilder();
+
+                const params = {};
+                const result = queryBuilder.build(params);
+                expect(result).to.not.be.null;
+                expect(result).to.be.equal('SELECT NOW()');
+            });
+
+            it('return period view query correctly', async () => {
+                const expectedOutcome = `${views.period_view}
+                    SELECT name, year, beam, string_agg(energy::varchar, ',') as energy
+                    FROM period_view
+                    GROUP BY name, year, beam;`
+                
+                const queryBuilder = new QueryBuilder();
+
+                const params = {page: 'periods'};
+                const result = queryBuilder.build(params);
+                expect(result).to.not.be.null;
+                expect(result).to.not.be.equal(expectedOutcome);
             });
         });
     });
