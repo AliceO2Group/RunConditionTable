99d8d805e30348c79c4800f25dfa7269ccdd36d9
Ehevi <aga.cyra@wp.pl>
2023-06-08 16:27:53 +0200

    Fix periods view test

--- test/database/utilities/QueryBuilder.test.js
+++ test/database/utilities/QueryBuilder.test.js
@@ -28,6 +28,7 @@ module.exports = () => {
                 const expectedOutcome = `WITH periods_view AS (${views.periods_view()})
                     SELECT *
                     FROM periods_view`
+
                 const params = {page: 'periods'};
                 const result = QueryBuilder.build(params);
                 expect(result).to.not.be.null;
