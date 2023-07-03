2f859e4429e4c8ae4b73f630d7a8f7a4dc6d81f8
Ehevi <aga.cyra@wp.pl>
2023-07-03 11:11:48 +0200

    Eslint fixes

--- app/lib/Utils.js
+++ app/lib/Utils.js
@@ -129,7 +129,9 @@ class Utils {
     static extractPeriodYear(name) {
         try {
             const year = parseInt(name.slice(3, 5), 10);
-            if (isNaN(year)) return 'NULL';
+            if (isNaN(year)) {
+                return 'NULL';
+            }
             return year > 50 ? year + 1900 : year + 2000;
         } catch (e) {
             return 'NULL';
