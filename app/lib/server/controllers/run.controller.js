de3a387e8a372b3c043b4ab8100fc9dfa49febee
xsalonx <lukasz.dubiel3@gmail.com>
2023-07-21 12:35:21 +0200

    refactor

--- app/lib/server/controllers/run.controller.js
+++ app/lib/server/controllers/run.controller.js
@@ -14,17 +14,13 @@
 const { databaseManager: { repositories: { RunRepository } } } = require('../../database');
 
 /**
- * Find all Attachments that belong to a certain log item
- *
- * @param {Object} req The *request* object represents the HTTP request and has properties for the request query
- *                         string, parameters, body, HTTP headers, and so on.
- * @param {Object} res The *response* object represents the HTTP response that an Express app sends when it gets an
- *                          HTTP request.
- * @param {Function} next The *next* object represents the next middleware function which is used to pass control to the
- *                        next middleware function.
- * @returns {undefined}
+ * List All runs in db
  */
-const listRuns = async (req, res, next) => res.json(await RunRepository.findAll()); // TODO DTO
+
+const listRuns = async (req, res, next) => {
+    const result = await RunRepository.findAll();
+    res.json(result);
+}; // TODO DTO
 
 module.exports = {
     listRuns,
