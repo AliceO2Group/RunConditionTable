12a122ea0fb3882dc7967d2c7e9c423a6ef73535
xsalonx <lukasz.dubiel3@gmail.com>
2023-07-24 10:54:17 +0200

    basic adapter and usecase for runs

---
+++ app/lib/usecases/GetAllRuns.js
@@ -0,0 +1,39 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+const {
+    repositories: {
+        RunRepository,
+    },
+} = require('../../database').databaseManager;
+const { runAdapter } = require('../database/adapters');
+
+/**
+ * GetAllRuns
+ */
+class GetRunUseCase {
+    /**
+     * Executes this use case.
+     *
+     * @param {Object} dto The GetRunsDto containing all data.
+     * @returns {Promise} Promise object represents the result of this use case.
+     */
+    // eslint-disable-next-line no-unused-vars
+    async execute(dto) {
+        const runs = await RunRepository.findAll();
+
+        return runs ? runs.map((run) => runAdapter.toEntity(run)) : null;
+    }
+}
+
+module.exports = GetRunUseCase;
