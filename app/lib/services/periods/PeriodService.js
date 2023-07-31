9853c7bde162c43b45d1b7ed8ad98e58ad450621
xsalonx <65893715+xsalonx@users.noreply.github.com>
2023-07-31 11:51:46 +0200

    basic of queryBuidler and Period Service

---
+++ app/lib/services/periods/PeriodService.js
@@ -0,0 +1,37 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+const {
+    databaseManager: {
+        repositories: {
+            PeriodRepository,
+        },
+    },
+} = require('../../database/DatabaseManager');
+const { periodAdapter } = require('../../database/adapters');
+
+class RunService {
+    /**
+     * Return all runs
+     * @param {Object} queryClauses - Sequelize query definiton with where clause,... #TODO
+     * @returns {Promise<Run[]>} Promise object represents the result of this use case.
+     */
+    async getAll(queryClauses) { // TODO args
+        const runs = await PeriodRepository.findAll(queryClauses);
+        return runs.map((run) => periodAdapter.toEntity(run));
+    }
+}
+
+module.exports = {
+    runService: new RunService(),
+};
