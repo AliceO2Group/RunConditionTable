53602857e35fad7893e686bdc4b98feed3bad221
Ehevi <aga.cyra@wp.pl>
2023-08-01 13:09:09 +0200

    Address review comments

--- app/public/utils/utils.js
+++ app/public/utils/utils.js
@@ -22,11 +22,11 @@ export const reduceSerialIf = (
 
 export function getReadableFileSizeString(fileSizeInBytes) {
     let i = -1;
-    const byteUnits = [' kB', ' MB', ' GB', ' TB', ' PB', ' EB', ' ZB', ' YB'];
+    const byteUnits = ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
     do {
         fileSizeInBytes /= 1024;
         i++;
     } while (fileSizeInBytes > 1024);
 
-    return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
+    return `${Math.max(fileSizeInBytes, 0.1).toFixed(1)}  ${byteUnits[i]}`;
 }
