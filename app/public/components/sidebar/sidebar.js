a45ac2f200e485bbb51750d9cde4e576aa260e1f
Ehevi <aga.cyra@wp.pl>
2023-08-03 10:19:23 +0200

    Cleanup :broom:

--- app/public/components/sidebar/sidebar.js
+++ app/public/components/sidebar/sidebar.js
@@ -26,48 +26,46 @@ const { pageNames } = RCT;
  */
 
 export default function sidebar(model) {
-    return h('.sidebar.p3', {
-        onmouseover: Array.from(document.getElementsByClassName('sidebar-section-title')),
-    },
-    modal(modalIds.about.modal),
-    modal(modalIds.pageSettings.modal, model),
-    h('.logo.hide-on-close'),
-    h('.flex-column.gap-20',
-        h('.sidebar-section',
-            h('.sidebar-section-title.hide-on-close', 'Pages'),
-            sidebarItem(model, pageNames.periods, 'Periods'),
-            sidebarItem(model, pageNames.dataPasses, 'Data Passes'),
-            sidebarItem(model, pageNames.anchoragePerDatapass, 'Anchorage per Data pass'),
-            sidebarItem(model, pageNames.mc, 'Monte Carlo'),
-            sidebarItem(model, pageNames.anchoredPerMC, 'Anchored per MC'),
-            sidebarItem(model, pageNames.runsPerPeriod, 'Runs per period'),
-            sidebarItem(model, pageNames.runsPerDataPass, 'Runs per Data pass'),
-            sidebarItem(model, pageNames.flags, 'QA flags')),
+    return h('.sidebar.p3',
+        modal(modalIds.about.modal),
+        modal(modalIds.pageSettings.modal, model),
+        h('.logo.hide-on-close'),
+        h('.flex-column.gap-20',
+            h('.sidebar-section',
+                h('.sidebar-section-title.hide-on-close', 'Pages'),
+                sidebarItem(model, pageNames.periods, 'Periods'),
+                sidebarItem(model, pageNames.dataPasses, 'Data Passes'),
+                sidebarItem(model, pageNames.anchoragePerDatapass, 'Anchorage per Data pass'),
+                sidebarItem(model, pageNames.mc, 'Monte Carlo'),
+                sidebarItem(model, pageNames.anchoredPerMC, 'Anchored per MC'),
+                sidebarItem(model, pageNames.runsPerPeriod, 'Runs per period'),
+                sidebarItem(model, pageNames.runsPerDataPass, 'Runs per Data pass'),
+                sidebarItem(model, pageNames.flags, 'QA flags')),
 
-        h('.sidebar-section',
-            h('.sidebar-section-title.hide-on-close', 'Preferences'),
-            h('button.sidebar-item-button', {
-                onclick: () => showModal(modalIds.pageSettings.modal) },
-            h('.flex-wrap.page-title',
-                h('.settings-20-off-white.vertical-center'),
-                h('.title-text.vertical-center.hide-on-close', 'Page settings'))),
+            h('.sidebar-section',
+                h('.sidebar-section-title.hide-on-close', 'Preferences'),
+                h('button.sidebar-item-button', {
+                    onclick: () => showModal(modalIds.pageSettings.modal) },
+                h('.flex-wrap.page-title',
+                    h('.settings-20-off-white.vertical-center'),
+                    h('.title-text.vertical-center.hide-on-close', 'Page settings'))),
 
-            h('button.sidebar-item-button', {
-                onclick: () => showModal(modalIds.about.modal),
-            },
-            h('.flex-wrap.page-title.justify-center.items-center',
-                h('.detector-15-off-white'),
-                h('.title-text.hide-on-close', 'dett'))),
+                h('button.sidebar-item-button', {
+                    onclick: () => showModal(modalIds.about.modal),
+                },
+                h('.flex-wrap.page-title.justify-center.items-center',
+                    h('.detector-15-off-white'),
+                    h('.title-text.hide-on-close', 'dett'))),
 
-            h('.flex-wrap.page-title.justify-center.items-center',
-                h('.detector-15-off-white.vertical-center'),
-                h('.title-text.vertical-center.hide-on-close', 'Detectors'))),
+                h('.flex-wrap.page-title.justify-center.items-center',
+                    h('.detector-15-off-white.vertical-center'),
+                    h('.title-text.vertical-center.hide-on-close', 'Detectors'))),
 
-        h('.sidebar-section',
-            h('button.sidebar-item-button', {
-                onclick: () => showModal(modalIds.about.modal),
-            },
-            h('.flex-wrap.page-title',
-                h('.about-15-off-white.vertical-center'),
-                h('.title-text.vertical-center.hide-on-close', 'About'))))));
+            h('.sidebar-section',
+                h('button.sidebar-item-button', {
+                    onclick: () => showModal(modalIds.about.modal),
+                },
+                h('.flex-wrap.page-title',
+                    h('.about-15-off-white.vertical-center'),
+                    h('.title-text.vertical-center.hide-on-close', 'About'))))));
 }
