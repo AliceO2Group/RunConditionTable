07e1366091901fa23869172e2c1ff2d648c5dd54
xsalonx <65893715+xsalonx@users.noreply.github.com>
2023-08-04 11:47:19 +0200

    SimulationPass service

---
+++ app/lib/services/simulationPasses/SimulationPassService.js
@@ -0,0 +1,56 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+const {
+    databaseManager: {
+        repositories: {
+            SimulationPassRepository,
+        },
+    },
+} = require('../../database/DatabaseManager');
+const { simulationPassAdapter } = require('../../database/adapters');
+const { filterToSequelizeWhereClause } = require('../../server/utilities');
+
+class DataPassService {
+    /**
+     * Return all data passes,
+     * @param {Object} query -  Filtering query definiton from http request
+     * @returns {Promise<SimulationPass[]>} Promise object represents the result of this use case.
+     */
+    async getAll({ filter }) {
+        const periods = await SimulationPassRepository.findAll({
+            where: filterToSequelizeWhereClause(filter),
+        });
+        return periods.map((dataPass) => simulationPassAdapter.toEntity(dataPass));
+    }
+
+    /**
+     * Return data passes belonging to period which id is provided
+     * @param {Number} periodId - id of period which for runs should be returnd
+     * @param {Object} query - Filtering query definiton from http request,... #TODO
+     * @returns {Promise<SimulationPass[]>} Promise object represents the result of this use case.
+     */
+    async getDataPassesPerPeriod(periodId, { filter }) {
+        const runs = await SimulationPassRepository.findAll({
+            where: {
+                period_id: periodId,
+                ...filterToSequelizeWhereClause(filter),
+            },
+        });
+        return runs.map((dataPass) => simulationPassAdapter.toEntity(dataPass));
+    }
+}
+
+module.exports = {
+    dataPassService: new DataPassService(),
+};
