ec1fa9d1d6e5371cca407b48d6598c9d6ed0676e
xsalonx <65893715+xsalonx@users.noreply.github.com>
2023-08-04 17:11:12 +0200

    add method in data pass service

--- app/lib/services/dataPasses/DataPassService.js
+++ app/lib/services/dataPasses/DataPassService.js
@@ -19,6 +19,7 @@ const {
     },
 } = require('../../database/DatabaseManager');
 const { dataPassAdapter } = require('../../database/adapters');
+const SimulationPass = require('../../database/models/SimulationPass');
 const { filterToSequelizeWhereClause } = require('../../server/utilities');
 
 class DataPassService {
@@ -44,6 +45,34 @@ class DataPassService {
         const runs = await DataPassRepository.findAll({
             where: {
                 period_id: periodId,
+                ...filterToSequelizeWhereClause(filter),
+            },
+        });
+        return runs.map((dataPass) => dataPassAdapter.toEntity(dataPass));
+    }
+
+    /**
+     * Return data passes which to period which id is provided
+     * @param {Number} simulationPassId - id of simulaton pass which for runs should be returnd
+     * @param {Object} query - Filtering query definiton from http request,... #TODO
+     * @returns {Promise<DataPass[]>} Promise object represents the result of this use case.
+     */
+    async getAnchorageForSimulationPass(simulationPassId, { filter }) {
+        const runs = await DataPassRepository.findAll({
+            include: [
+                {
+                    model: SimulationPass,
+                    require: true,
+                    attributes: [],
+                    through: {
+                        where: {
+                            sim_pass_id: simulationPassId,
+                        },
+                    },
+                },
+            ],
+
+            where: {
                 ...filterToSequelizeWhereClause(filter),
             },
         });
