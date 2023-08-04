1499e407ba676ad8387c3287eda6b5d06c01c3c3
xsalonx <65893715+xsalonx@users.noreply.github.com>
2023-08-04 09:34:58 +0200

    updates mathod

--- app/lib/services/runs/RunService.js
+++ app/lib/services/runs/RunService.js
@@ -44,7 +44,7 @@ class RunService {
      * @returns {Promise<Run[]>} Promise object represents the result of this use case.
      */
     async getRunsPerPeriod(periodId, { filter }) {
-        const runs = await RunRepository.findAll({
+        const runs = await RunRepository.findAllWithDetectors({
             where: {
                 period_id: periodId,
                 ...filterToSequelizeWhereClause(filter),
@@ -60,7 +60,7 @@ class RunService {
      * @returns {Promise<Run[]>} Promise object represents the result of this use case.
      */
     async getRunsPerDataPass(dataPassId, { filter }) {
-        const runs = await RunRepository.findAll({
+        const runs = await RunRepository.findAllWithDetectors({
             include: [
                 {
                     model: DataPass,
