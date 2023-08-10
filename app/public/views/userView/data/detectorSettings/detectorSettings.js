696e47d476219fd9cd25ef9796ee2ffda3f6aeb1
Ehevi <aga.cyra@wp.pl>
2023-08-10 11:18:34 +0200

    Cleanup :broom:

--- app/public/views/userView/data/detectorSettings/detectorSettings.js
+++ app/public/views/userView/data/detectorSettings/detectorSettings.js
@@ -15,35 +15,19 @@
 import { h } from '/js/src/index.js';
 
 export default function detectorSettings(userPreferences, close) {
-    const rowsOnSiteInputId = 'rows-per-page-input-id-modal';
-
     const title = h('h3.text-primary', 'Detector list');
 
-    const onclickApply = (userPreferences) => {
-        const input = document.getElementById(rowsOnSiteInputId);
-        const inputValue = input.value === '' ? input.placeholder : input.value;
-        if (inputValue < 1 || inputValue > 200) {
-            alert('incorrect number of rows on page: must be in range of [1, 200]');
-            input.value = userPreferences.rowsOnSite;
-        } else {
-            userPreferences.setRowsOnSite(inputValue);
-        }
-        close();
-    };
-
-    const detectors = () => {
-        return h('.text-dark-blue', Object.keys(userPreferences.detectorList).map((detector) => [
-            h('.flex-wrap.justify-between.items-center',
-                h('.text-dark-blue', detector),
-                h('input.toggle-switch', {
-                    type: 'checkbox',
-                    onclick: () => {
-                        userPreferences.changeDetectorVisibility(detector);
-                    },
-                    checked: userPreferences.detectorList[detector] === true,
-                })),
-        ]));
-    };
+    const detectors = () => h('.text-dark-blue', Object.keys(userPreferences.detectorList).map((detector) => [
+        h('.flex-wrap.justify-between.items-center',
+            h('.text-dark-blue', detector),
+            h('input.toggle-switch', {
+                type: 'checkbox',
+                onclick: () => {
+                    userPreferences.changeDetectorVisibility(detector);
+                },
+                checked: userPreferences.detectorList[detector] === true,
+            })),
+    ]));
 
     return h('.p-1em', [
         h('.flex.p-bottom-1em.justify-center.items-center',
@@ -54,7 +38,7 @@ export default function detectorSettings(userPreferences, close) {
 
         h('.flex-wrap.justify-center.items-center.p-1em.p-bottom-0',
             h('button.btn.btn-primary', {
-                onclick: () => onclickApply(userPreferences),
+                onclick: () => close(),
             }, 'Apply changes')),
     ]);
 }
