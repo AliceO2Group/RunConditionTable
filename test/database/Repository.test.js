80251ee04e299ec318a997b94bdad00d5b07068c
xsalonx <65893715+xsalonx@users.noreply.github.com>
2023-08-10 13:47:33 +0200

    add transactional repositories tests

---
+++ test/database/Repository.test.js
@@ -0,0 +1,34 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+const { databaseManager: { repositories } } = require('../../app/lib/database/DatabaseManager.js'); 
+const assert = require('assert');
+
+module.exports = () => {
+    describe('RepositoriesSuite', () => {
+        describe('testing if transaction methods give the same result as non-transactional ones', () => {
+            const testableMethodNames = new Set(['count', 'findAll', 'findOne']);
+            Object.values(repositories)
+                .map((repo) => Object.getOwnPropertyNames(repo.T)
+                                    .filter(n => testableMethodNames.has(n))
+                                    .map((methodName) => [repo, methodName]))
+            .flat()
+            .map(([repo, methodName]) => {
+                it(`should acquire the same result from transaction method and non-transactional one for repository ${repo.model.name}' and method '${methodName}'`, async () => {
+                    const nonTransactionResult = await repo[methodName]();
+                    const transationResult = await repo.T[methodName]();
+                    assert.deepStrictEqual(nonTransactionResult, transationResult);
+                })
+            });
+        });
+    });
+};
