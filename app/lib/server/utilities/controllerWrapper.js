fa49ca3ec2e1da5e2c1b8603d312394d8ab37a5d
xsalonx <65893715+xsalonx@users.noreply.github.com>
2023-08-15 13:42:20 +0200

    amend controller wrapper

--- app/lib/server/utilities/controllerWrapper.js
+++ app/lib/server/utilities/controllerWrapper.js
@@ -14,7 +14,7 @@
 const controllerHandlerWrapper = (controllerHandler) =>
     async (req, res, next) =>
         await controllerHandler(req, res, next)
-            .catch((err) => res.status(400).json({ error: err.stack }));
+            .catch((err) => res.status(400).json({ error: err.message }));
 
 module.exports = {
     controllerHandlerWrapper,
