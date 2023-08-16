146548adedcf91dcc0fa9476185bc3f5be041488
xsalonx <65893715+xsalonx@users.noreply.github.com>
2023-08-16 14:05:08 +0200

    add fillNumber to Run model

---
+++ app/lib/database/migrations/20230816140700-create-runs_detectors.js
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('runs_detectors', {
+                quality: {
+                    type: Sequelize.STRING,
+                },
+                run_number: {
+                    type: Sequelize.INTEGER,
+                    primaryKey: true,
+                    references: {
+                        model: 'runs',
+                        key: 'run_number',
+                    },
+                },
+                detector_id: {
+                    type: Sequelize.INTEGER,
+                    primaryKey: true,
+                    references: {
+                        model: 'detectors_subsystems',
+                        key: 'id',
+                    },
+                },
+            },
+        )
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('runs_detectors');
+    }
+};
---
+++ app/lib/database/migrations/20230816140800-create-flag_types_dictionary.js
@@ -0,0 +1,50 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('flag_types_dictionary', {
+                id: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                    primaryKey: true,
+                    autoIncrement: true,
+                },
+                name: {
+                    type: Sequelize.STRING,
+                    unique: true,
+                    allowNull: false,
+                },
+                method: {
+                    type: Sequelize.STRING,
+                    unique: true,
+                    allowNull: false,
+                },
+                bad: {
+                    type: Sequelize.BOOLEAN,
+                    unique: true,
+                    allowNull: false,
+                },
+                obsolate: {
+                    type: Sequelize.BOOLEAN,
+                    unique: true,
+                    allowNull: false,
+                },
+            },
+        )
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('flag_types_dictionary');
+    }
+};
