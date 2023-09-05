046b3c5c95972c227fb64bdf7a78e09fb86b9c04
xsalonx <lukasz.dubiel3@gmail.com>
2023-09-05 11:22:28 +0200

    hooks update

--- hooks/pre-commit/newlines.sh
+++ hooks/pre-commit/newlines.sh
@@ -7,12 +7,16 @@ done;
 FIND_EXCLUDE_PHRASE="$FIND_EXCLUDE_PHRASE -not -path './.git/*'"
 FIND_EXCLUDE_PHRASE="$FIND_EXCLUDE_PHRASE -not -path '*.png'"
 FIND_EXCLUDE_PHRASE="$FIND_EXCLUDE_PHRASE -not -path './app/public/styles/images/favicon/favicon.ico'" # it seems to be binary but for some reason `find ... -type f` finds it 
+FIND_EXCLUDE_PHRASE="$FIND_EXCLUDE_PHRASE -not -path './test/lib/alimonitor-services/cache/*'"
+
 FINDCMD="find . -type f $FIND_EXCLUDE_PHRASE"
 
 f() {
     for p in $(bash -c "$FINDCMD"); do
-        test $(tail -c1 "$p" | tr $'\n' '_') != '_' && echo "No newline at end of $p";
-        test "$(tail -c2 "$p" | tr $'\n' '_')" = '__' && echo "More than one newline at the end of $p";
+        if [ $(echo $p | grep "$(git ls-files -md; git diff --cached --name-only)") ]; then
+            test $(tail -c1 "$p" | tr $'\n' '_') != '_' && echo "No newline at end of $p";
+            test "$(tail -c2 "$p" | tr $'\n' '_')" = '__' && echo "More than one newline at the end of $p";
+        fi;
     done;
 }
 
