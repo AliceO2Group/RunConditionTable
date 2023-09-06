9d16f66d618675f768548fa66c3ad5896877449c
xsalonx <65893715+xsalonx@users.noreply.github.com>
2023-09-06 11:26:49 +0200

    one more hook

--- test/lib/alimonitor-services/syncManager.test.js
+++ test/lib/alimonitor-services/syncManager.test.js
@@ -12,25 +12,40 @@
  * or submit itself to any jurisdiction.
  */
 
+const { rctData: { detectors } } = require('../../../app/lib/config/configProvider.js');
 const { syncManager } = require('../../../app/lib/alimonitor-services/SyncManager.js');
-const { databaseManager } = require('../../../app/lib/database/DatabaseManager.js');
+const { databaseManager: { repositories: {
+    RunRepository,
+    RunDetectorsRepository,
+    DetectorSubsystemRepository,
+},
+} } = require('../../../app/lib/database/DatabaseManager.js');
 const { generateRandomBookkeepingCachedRawJsons } = require('./testutil/cache-for-test.js');
 const assert = require('assert');
 
 module.exports = () => describe('SyncManager suite', () => {
+    before('should fetch detectors data from DB the same as in config', async () => await DetectorSubsystemRepository
+        .findAll({ raw: true })
+        .then((detectoSubsystemData) => detectoSubsystemData.map(({ name }) => name))
+        .then((detectoSubsystemNames) => assert.deepStrictEqual(detectoSubsystemNames.sort(), detectors.sort())));
+
     describe('BookkeepingService suite', () => {
         before(() => {
             generateRandomBookkeepingCachedRawJsons();
         });
 
-        after(async () => {
-            await databaseManager.repositories.RunRepository
-                .findAll({ raw: true })
-                .then((data) => assert(data.length > 0));
-        });
-
         it('should performe sync with random data withour major errors', async () => {
             assert.strictEqual(await syncManager.services.bookkeepingService.setSyncTask(), true);
         });
+
+        it('should fetch some run data directly from DB', async () =>
+            await RunRepository
+                .findAll({ raw: true })
+                .then((data) => assert(data.length > 0)));
+
+        it('should fetch some run_detector data directly from DB', async () =>
+            await RunDetectorsRepository
+                .findAll({ raw: true })
+                .then((data) => assert(data.length > 0)));
     });
 });
