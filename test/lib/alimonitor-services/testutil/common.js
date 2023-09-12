fdbd344892927a3d0455a3922761d71631a4f7ee
xsalonx <lukasz.dubiel3@gmail.com>
2023-09-12 20:17:51 +0200

    docs

--- test/lib/alimonitor-services/testutil/common.js
+++ test/lib/alimonitor-services/testutil/common.js
@@ -15,29 +15,52 @@
 const randint = (min = 0, max = 0) => Math.round(Math.random() * (max - min) + min);
 const choice = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-const universalNoncontextualDataUnitGenerator = (unitGenerator) => {
-    if (typeof unitGenerator === 'function') {
-        return unitGenerator();
-    } else if (typeof unitGenerator === 'object') {
-        return Array.isArray(unitGenerator) ?
-            unitGenerator.map((subUnitGenerator) => universalNoncontextualDataUnitGenerator(subUnitGenerator))
+/**
+ * Generate random data with structure defined in given argument
+ * @param {*} unitDefinition - definition of data to be generated
+ * @return {*} data
+ * UnitGenerator can be any strcture, primitive value, function or their composition
+ * How does the function work can be described via produtions:
+ * if unitDefinition is ... then the function (abbr. here to U) returns ...:
+ *  U: PRIMITIVE_VALUE -> PRIMITIVE_VALUE
+ *  U: FUNCTION -> FUNCTION()
+ *  U: ARRAY<SUB_UNIT_DEFINITION> -> ARRAY<U(SUB_UNIT_DEFINITION)>
+ *  U: OBJECT<K, V> -> OBJECT<U(K),U(V)>
+ *
+ * In particular, in last case key of some unitDefinition being object
+ * can be a function generating some random key, but not being given explicitly, rather as wrapped one with class 'Symbol',
+ * so if you want to have data (objects) with random keys you can provide unitDefinition like
+ * {[Symbol(() => SOME_RANDOM_KEY)]: () => SOME_RANDOM_VALUE, ...}
+ */
+const universalNoncontextualDataUnitGenerator = (unitDefinition) => {
+    if (typeof unitDefinition === 'function') {
+        return unitDefinition();
+    } else if (typeof unitDefinition === 'object') {
+        return Array.isArray(unitDefinition) ?
+            unitDefinition.map((subUnitDefinition) => universalNoncontextualDataUnitGenerator(subUnitDefinition))
             : Object.fromEntries([
-                ...Object.entries(unitGenerator).map(([keyName, subUnitGenerator]) =>
+                ...Object.entries(unitDefinition).map(([keyName, subUnitGenerator]) =>
                     [keyName, universalNoncontextualDataUnitGenerator(subUnitGenerator)]),
-                ...Object.getOwnPropertySymbols(unitGenerator)
+                ...Object.getOwnPropertySymbols(unitDefinition)
                     .map((symbol) => [
-                        eval(symbol.description)(),
-                        universalNoncontextualDataUnitGenerator(unitGenerator[symbol]),
+                        universalNoncontextualDataUnitGenerator(eval(symbol.description)),
+                        universalNoncontextualDataUnitGenerator(unitDefinition[symbol]),
                     ]),
             ]);
     } else {
         // Assume some primitive type
-        return unitGenerator;
+        return unitDefinition;
     }
 };
 
-const universalNoncontextualArrayDataGenerator = (size, unitGenerator) =>
-    [...new Array(size)].map(() => universalNoncontextualDataUnitGenerator(unitGenerator));
+/**
+ * Generate array with given size, which elements are generated according to given unitDefinition
+ * @param {Number} size - returning array size
+ * @param {*} unitDefinition - @see universalNoncontextualDataUnitGenerator
+ * @return {Array<*>} array of genereated data
+ */
+const universalNoncontextualArrayDataGenerator = (size, unitDefinition) =>
+    [...new Array(size)].map(() => universalNoncontextualDataUnitGenerator(unitDefinition));
 
 module.exports = {
     randint,
