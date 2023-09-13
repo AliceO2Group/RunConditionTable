212419c5663619987864d51765153a0785ad682d
xsalonx <65893715+xsalonx@users.noreply.github.com>
2023-09-13 14:48:47 +0200

    refactor

--- test/lib/alimonitor-services/testutil/bookkeeping-cache-test-data.js
+++ test/lib/alimonitor-services/testutil/bookkeeping-cache-test-data.js
@@ -3,11 +3,11 @@ const path = require('path');
 const { Cacher } = require('../../../../app/lib/alimonitor-services/helpers');
 const { rctData: { detectors } } = require('../../../../app/lib/config/configProvider.js');
 
-const { randint, choice, universalNoncontextualArrayDataGenerator, randomPeriodName } = require('./common.js');
+const { randint, choice, universalNoncontextualArrayDataGenerator, randomPeriodName, randomRunNumber, randomBeamType } = require('./common.js');
 
 const dataUnitDefinition = {
-    runNumber: () => randint(1000000, 9000000),
-    lhcPeriod: () => randomPeriodName(),
+    runNumber: randomRunNumber,
+    lhcPeriod: randomPeriodName,
     timeO2Start: () => randint(100000000, 200000000),
     timeO2End: () => randint(100000000, 200000000),
     timeTrgStart: () => randint(200000000, 300000000),
@@ -20,7 +20,7 @@ const dataUnitDefinition = {
     aliceDipoleCurrent: () => randint(10000, 20000),
     aliceDipolePolarity: () => choice(['POSITIVE', 'NEGATIVE']),
     fillNumber: () => randint(5000, 9000),
-    pdpBeamType: () => choice(['pp', 'PbPb', 'pPb']),
+    pdpBeamType: randomBeamType,
 };
 
 const getBkpSourceFiles = (offset, limit) =>
