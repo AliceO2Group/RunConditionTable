6302ec7f7faf3757e23177ebd08e4eee3e3e180b
xsalonx <65893715+xsalonx@users.noreply.github.com>
2023-09-14 14:32:14 +0200

    docs

--- app/lib/server/utilities/filterParser.js
+++ app/lib/server/utilities/filterParser.js
@@ -132,80 +132,123 @@ class TransformHelper {
 }
 
 /**
+ * !NOTE for pattern notation used in this description:
+ * uppercase written words are non-terminal (as in formal gramatics terminology)
+ * and the lowercase one are terminals, e.g. operator 'not' is terminal):
+ * dots separate non-terminals (just for clarity),
+ * start is Kleen start (as in posix regex)
+ * question mark is option quantifier (as in posix regex)
+ *
  * Transform filter object from http request.query to sequelize where-clause object
  * Considering the filter object is a tree, each root -> leaf path can be described as:
- * 1. one of the two patterns
- * (dots separate nodes, uppercase written words are non-terminal (as in formal gramatics terminology)
- * and that lowercase are terminal, e.g. operator 'not' is terminal):
  *
- *   (.LOGIC_OPERATR)*.DB_FIELD_NAME.(RELATION_OPERATOR.VALUE | GROUP_OPERATOR.(LOGIC_OPERATOR)) | or
- *   (.LOGIC_OPERATR)*.GROUP_OPERATOR.(LOGIC_OPERATOR)
+ *      FILTER_PATH -> .LOGIC_PATH .CONDITION_TAIL
  *
- *      where '_goperator' is the only GROUP_OPERATOR terminal,
+ * where .LOGIC_PATH and .CONDITION_TAIL are
  *
- * So each top-down path begins with sequence of logical operators and ends with
- *         DB_FIELD_NAME._fgoperator.(and|or|not)
- * or with DB_FIELD_NAME.RELATIONAL_OPERATOR.VALUE
- * or with ._fgoperator.(and|or|not)
+ *      .LOGIC_PATH -> ( .LOGIC_OPERATOR | .ARRAY_ELEM_DESCRIPTOR | (.GROUP_OPERATOR .LOGIC_OPERATOR) )*
+ *      .CONDITION_TAIL -> .DB_FIELD_NAME ( (.GROUP_OPERATOR .LOGIC_OPERATOR) | .RELATIONAL_OPERATOR)? .VALUES
  *
- * 2. one of the two corresnponding regexes:
- *   (\.((and)|(or)|(not)))*\.([_a-z][_a-z0-9]*)\.(((and)|(or)|(not))\..*)|(_fgoperator\.((or)|(and)|(not))))                |
- *   -----------------------  ------------------   -----------------------|--------------------------------                  | or
- *       (.LOGIC_OPERATR)*      .DB_FIELD_NAME     RELATION_OPERATOR.VALUE   GROUP_OPERATOR.LOGIC_OPERATOR                   |
+ *      where for GROUP_OPERATOR nonterminal allowed terminals are: '_goperator',
  *
- *   (\.((and)|(or)|(not)))*\._fgoperator\.((or)|(and)|(not))
- *   -----------------------  --------------------------------
- *       (.LOGIC_OPERATR)*     .GROUP_OPERATOR.LOGIC_OPERATOR
+ *            for LOGIC_OPERATOR nonterminal allowed terminals are 'and', 'or', 'not',
+ *
+ *            for ARRAY_ELEM_DESCRIPTOR nonterminal allowed terminals are each which match following string ^\$((0)|([1-9]+[0-9]*))
+ *                  ,so dollar ($) character followed by any non-negative integer
+ *
+ *            for RELATIONAL_OPERATOR nonterminal allowed terminals are
+ *                      'gt', 'gte', 'lt', 'lte', 'ne',
+ *                      'in', 'notIn', 'between', 'notBetween',
+ *                      'like', 'notLike', 'iLike', 'notILike', 'regexp', 'notRegexp', 'iRegexp', 'notIRegexp'
+ *
+ *            for DB_FIELD_NAME nonterminal allowed terminal is any identifier [_a-z]+[_a-z0-9]* which not overlap with previous ones
+ *            for VALUES nonterminal allowed terminal is anything what is meaningful for preceding relational operator
  *
  *
- * 3. As this api is intended for handling http requests in which filtering is defined by url query params,
+ * As this api is intended for handling http requests in which filtering is defined by url query params,
  * the GROUP_OPERATOR is syntax sugar:
- * Instead of /?filter[field1][or][gt]=10&filter[field1][or][lt]=3
+ * Instead of
+ *
+ *          .../?filter[field1][or][gt]=10&filter[field1][or][lt]=3
  * it can be
- *            /?filter[field1][gt]=10&filter[field][lt]=3&filter[field1][_fgoperator]=or
+ *
+ *          .../?filter[field1][gt]=10&filter[field][lt]=3&filter[field1][_fgoperator]=or
  * GROUP_OPERATOR can be ommited, its default value is an 'and'.
  *
  * Example:
- * 1` URL is: .../?filter[or][field1][gt]=10&filter[or][field1][lt]=3&filter[or][field1][_goperator]=or ...
- *              ... &filter[or][filed2][like]=LHC_%pass&filter[or][filed2][notLike]=LHC_c%
- * 2` The url will be parsed by qs in express to:
- * const query = {
- * "filter": {
- *   "or": {
- *     "field1": {
- *       "gt": "10",
- *       "lt": "3",
- *       "_goperator": "or"
- *     },
- *     "filed2": {
- *       "like": "LHC_%pass",
- *       "notLike": "LHC_c%"
- *     }
- *   }
- * }
  *
- * 3` query.filter is being passed as an argument to filterToSequelizeWhereClause function
+ * 1 URL search params are:
+ *
+ *        .../?filter[or][field1][gt]=10&filter[or][field1][lt]=3&filter[or][field1][_goperator]=or
+ *            &filter[or][filed2][like]=LHC_%25pass&filter[or][filed2][notLike]=LHC_c%25
+ *            &filter[and][$1][or][f1]=1&filter[and][$1][or][f2]=2&filter[and][$2][or][f3]=3&filter[and][$2][or][f4]=5
+ *
+ * 2 The url will be parsed by qs in express to:
+ *
+ *      const query = {
+ *      "filter": {
+ *        "or": {
+ *          "field1": {
+ *            "gt": "10",
+ *            "lt": "3",
+ *            "_goperator": "or"
+ *          },
+ *          "filed2": {
+ *            "like": "LHC_%pass",
+ *            "notLike": "LHC_c%"
+ *          }
+ *        },
+ *        "and": {
+ *           "$1": {
+ *               "or": {
+ *                   f1: 1,
+ *                   f2: 2,
+ *               }
+ *           }
+ *           "$2": {
+ *               "or": {
+ *                   f3: 3,
+ *                   f4: 4,
+ *               }
+ *           }
+ *        }
+ *      }
+ *
+ * 3 query.filter is being passed as an argument to filterToSequelizeWhereClause function
  * which returns sequelize-compatible where-clause
  *
- * {
- *  [Symbol(or)]: {
- *    field1: {
- *      [Symbol(or)]: {
- *        [Symbol(gt)]: '10',
- *        [Symbol(lt)]: '3'
+ *      {
+ *         [Symbol(or)]: {
+ *           field1: {
+ *             [Symbol(or)]: {
+ *               [Symbol(gt)]: '10',
+ *               [Symbol(lt)]: '3'
+ *             }
+ *           },
+ *           filed2: {
+ *             [Symbol(and)]: {
+ *               [Symbol(like)]: 'LHC_%pass',
+ *               [Symbol(notLike)]: 'LHC_c%'
+ *             }
+ *           }
+ *         },
+ *
+ *         [Symbol(and)]: [      !!! Note that here is square bracket
+ *            [Symbol(or)]: {
+ *               f1: 1,
+ *               f2: 2,
+ *            },
+ *            [Symbol(or)]: {
+ *               f3: 3,
+ *               f4: 4,
+ *            }
+ *         ]
  *      }
- *    },
- *    filed2: {
- *      [Symbol(and)]: {
- *        [Symbol(like)]: 'LHC_%pass',
- *        [Symbol(notLike)]: 'LHC_c%'
- *      }
- *    }
- *  }
- * }
  *
  * What is equivalent to sql:
- * WHERE (field1 > 10 OR field1 < 3) OR (field2 like 'LHC_%pass' AND field2 NOT LIKE 'LHC_c%')
+ *
+ *      WHERE ((field1 > 10 OR field1 < 3) OR (field2 like 'LHC_%pass' AND field2 NOT LIKE 'LHC_c%'))
+ *        AND ( ( f1 = 1 OR f2 = 2 ) AND ( f3 = 3 OR f4 = 4 ) )
  *
  * @param {Object} filter - from req.query
  * @param {boolean} pruneRedundantANDOperator - if true (default) remove unnecessaary 'and' operators
