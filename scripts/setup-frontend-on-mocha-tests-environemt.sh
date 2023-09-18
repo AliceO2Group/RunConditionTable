40a8a5adfcaba20f05b75f9ce408adc8e92c9eec
xsalonx <65893715+xsalonx@users.noreply.github.com>
2023-09-18 11:10:47 +0200

    mockup of renderers

--- scripts/setup-frontend-on-mocha-tests-environemt.sh
+++ scripts/setup-frontend-on-mocha-tests-environemt.sh
@@ -2,9 +2,24 @@
 
 if [ "$RUNNING_ENV" != 'DOCKER' ]; then
     echo \
-    "Warning this script is rather intended
-    to setup environment in docker contatiner.
-    If it is docker contatiner check wheter
+    "Warning this script is intended
+    to setup environment in docker container.
+    If it is docker container check whether
     env var RUNNING_ENV has proper value assign,
     it should be 'DOCKER'";
+    exit 1;
 fi
+
+
+NODE_MODULES_DIR_PATH=${1:-$(dirname $0)/../node_modules}
+if [ -n "$NODE_MODULES_DIR_PATH" ]; then
+    echo "Path to node_modules not specified, it must be given as first argument";
+fi
+
+FRONTEND_DIR="$NODE_MODULES_DIR_PATH/\@aliceo2/web-ui/Frontend/";
+
+FILE_TO_BE_REPLACED_PATH="/js/src/renderer.js";
+SRC_MOCHA_CODE=$(dirname $0)/web-ui-renderers.mocha.js;
+
+cp -r $FRONTEND_DIR/js /js;
+cp $SRC_MOCHA_CODE $FILE_TO_BE_REPLACED_PATH;
