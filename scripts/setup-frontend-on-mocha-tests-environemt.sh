ed22ea70549fc016b00c6b6ddb77662444d2611f
xsalonx <65893715+xsalonx@users.noreply.github.com>
2023-09-18 13:38:23 +0200

    defined procedure for serving forntend files to mocha on backend

--- scripts/setup-frontend-on-mocha-tests-environemt.sh
+++ scripts/setup-frontend-on-mocha-tests-environemt.sh
@@ -11,15 +11,40 @@ if [ "$RUNNING_ENV" != 'DOCKER' ]; then
 fi
 
 
-NODE_MODULES_DIR_PATH=${1:-$(dirname $0)/../node_modules}
-if [ -n "$NODE_MODULES_DIR_PATH" ]; then
+NODE_MODULES_DIR_PATH=${1:-$(dirname $(realpath $0) )/../node_modules}
+if [ -z "$NODE_MODULES_DIR_PATH" ]; then
     echo "Path to node_modules not specified, it must be given as first argument";
 fi
 
-FRONTEND_DIR="$NODE_MODULES_DIR_PATH/\@aliceo2/web-ui/Frontend/";
+FRONTEND_DIR="$NODE_MODULES_DIR_PATH/@aliceo2/web-ui/Frontend/";
 
-FILE_TO_BE_REPLACED_PATH="/js/src/renderer.js";
 SRC_MOCHA_CODE=$(dirname $0)/web-ui-renderers.mocha.js;
 
+
+
+NODEJS_WINDOW_CODE="
+// Provide pseudo window object with uploaded mithril
+const m = () => ({});
+m.render = () => ({});
+const window = {
+    location: new URL('http://localhost:8081/?personid=0&name=_&token=_&username=_&access=_'),
+    history: {
+        replaceState: () => ({}),
+    },
+    m,
+    requestAnimationFrame: () => ({}),
+    cancelAnimationFrame: () => ({}),
+};
+"
+
+rm -rf /js
 cp -r $FRONTEND_DIR/js /js;
-cp $SRC_MOCHA_CODE $FILE_TO_BE_REPLACED_PATH;
+
+TARGET_SRC_DIR=/js/src/
+for fp in $(ls $TARGET_SRC_DIR ); do
+    FPATH=$TARGET_SRC_DIR/$fp;
+    echo "$NODEJS_WINDOW_CODE" $'\n' "$(cat $FPATH)" > $FPATH;
+done;
+
+FILE_TO_BE_REPLACED_PATH="/js/src/renderer.js";
+echo "$( cat "$FILE_TO_BE_REPLACED_PATH" | grep -v "import '/mithril/mithril.min.js'" )" > "$FILE_TO_BE_REPLACED_PATH";
