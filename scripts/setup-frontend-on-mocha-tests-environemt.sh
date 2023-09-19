f56b2218cba7ff8de581c98c0494c835c78423e5
xsalonx <65893715+xsalonx@users.noreply.github.com>
2023-09-19 09:05:35 +0200

    docs, usage

--- scripts/setup-frontend-on-mocha-tests-environemt.sh
+++ scripts/setup-frontend-on-mocha-tests-environemt.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+usage() {
+    cat << USAGE >&2
+
+The script adjust @aliceo2/web-ui frontend dependencies
+to be used within backend unit test environment with mocha.
+The only and the first (optional) argument is path
+to node_modules directory where the framework is installed.
+It is resolved to <THIS_SCRIPT_DIR>/../node_modules by default.
+
+USAGE
+exit 1;
+}
+
+
 if [ "$RUNNING_ENV" != 'DOCKER' ]; then
     echo \
-    "Warning this script is intended
+    "Warning!!! This script is intended
     to setup environment in docker container.
-    If it is docker container check whether
-    env var RUNNING_ENV has proper value assign,
-    it should be 'DOCKER'";
+    If, indeed, it was an attempt to run on docker
+    container check whether env var RUNNING_ENV has
+    proper value assign, it should be 'DOCKER' ";
     exit 1;
 fi
 
@@ -38,7 +52,9 @@ const window = {
 "
 
 rm -rf /js
-cp -r $FRONTEND_DIR/js /js;
+if ! cp -r $FRONTEND_DIR/js /js; then
+    usage
+fi
 
 TARGET_SRC_DIR=/js/src/
 for fp in $(ls $TARGET_SRC_DIR ); do
