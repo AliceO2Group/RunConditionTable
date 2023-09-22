36ad54069c9c30ba05d7f3ba4897b299f88ee42c
xsalonx <65893715+xsalonx@users.noreply.github.com>
2023-09-22 13:09:10 +0200

    amend legacy db view

--- app/lib/database/views/runs_views.js
+++ app/lib/database/views/runs_views.js
@@ -20,8 +20,8 @@
 const queryForRunsFields = `
         --p.name, 
         r.run_number, 
-        r.time_start, 
-        r.time_end, 
+        r.time_o2_start, 
+        r.time_o2_end, 
         r.time_trg_start, 
         r.time_trg_end,
         get_center_of_mass_energy(r.energy_per_beam, p.beam_type_id) as center_of_mass_energy, 
