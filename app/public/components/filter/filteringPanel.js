5a786eacdefd043e06e2a94b251e78958b52a31c
Ehevi <aga.cyra@wp.pl>
2023-09-22 11:45:32 +0200

    Split filter panel

--- app/public/components/filter/filteringPanel.js
+++ app/public/components/filter/filteringPanel.js
@@ -23,11 +23,16 @@ const { filterTypes } = RCT;
  * @returns {vnode} filter panel
  */
 export default function filteringPanel(model) {
+    const fieldSelectId = 'filter-field-select';
+    const typeSelectId = 'filter-type-select';
+    const leftInputId = 'filter-left-input';
+    const rightInputId = 'filter-right-input';
+
     const fields = model.visibleFields;
     const aggregatedFiltersTypes = `${filterTypes.match}-${filterTypes.exclude}`;
 
     const onFilteringTypeChange = () => {
-        const filteringTypeSelect = document.getElementById('filters-opts-select');
+        const filteringTypeSelect = document.getElementById(typeSelectId);
         const selectedType = filteringTypeSelect.options[filteringTypeSelect.selectedIndex].value;
         const types = selectedType == filterTypes.between ? ['from', 'to'] : [filterTypes.match, filterTypes.exclude];
 
@@ -37,21 +42,21 @@ export default function filteringPanel(model) {
     };
 
     const clearUserInput = () => {
-        document.getElementById('show-options-field').value = '';
-        document.getElementById('filters-opts-select').value = '';
-        document.getElementById('left-filter-input').value = '';
-        document.getElementById('right-filter-input').value = '';
+        document.getElementById(fieldSelectId).value = '';
+        document.getElementById(typeSelectId).value = '';
+        document.getElementById(leftInputId).value = '';
+        document.getElementById(rightInputId).value = '';
     };
 
     const onFilterSubmit = async () => {
-        const fieldNameSelect = document.getElementById('show-options-field');
+        const fieldNameSelect = document.getElementById(fieldSelectId);
         const fieldNameValue = fieldNameSelect.options[fieldNameSelect.selectedIndex].value;
 
         const leftFilterType = document.getElementById('left-filter-placeholder').innerHTML;
         const rightFilterType = document.getElementById('right-filter-placeholder').innerHTML;
 
-        const leftFilterInput = document.getElementById('left-filter-input').value;
-        const rightFilterInput = document.getElementById('right-filter-input').value;
+        const leftFilterInput = document.getElementById(leftInputId).value;
+        const rightFilterInput = document.getElementById(rightInputId).value;
 
         if (rightFilterInput) {
             model.filtering.addFilter(fieldNameValue, rightFilterInput, rightFilterType);
@@ -64,6 +69,50 @@ export default function filteringPanel(model) {
         clearUserInput();
     };
 
+    const fieldSelect = h('select.select', {
+        id: fieldSelectId,
+        name: 'showOptions' }, [
+        h('option', {
+            value: '',
+            selected: true,
+            disabled: true,
+            hidden: true,
+        }, 'Field'),
+        fields.map((field) => h('option', { value: field.name }, field.fieldName)),
+    ], h('.close-10-primary'));
+
+    const typeSelect = h('select.select', {
+        id: typeSelectId,
+        name: 'showOptions',
+        onchange: () => onFilteringTypeChange(),
+    }, [
+        h('option', { value: '', selected: true, disabled: true, hidden: true }, 'Filtering type'),
+        h('option', { value: aggregatedFiltersTypes }, aggregatedFiltersTypes),
+        h('option', { value: filterTypes.between }, filterTypes.between),
+    ], h('.close-10-primary'));
+
+    const leftInput = h('.text-field',
+        h('input.form-control.relative', {
+            style: 'width:120px',
+            type: 'text',
+            value: document.getElementById(typeSelectId)?.options[Selection.selectedIndex]?.value,
+            disabled: false,
+            id: leftInputId,
+            required: true,
+        }),
+        h('span.placeholder', { id: 'left-filter-placeholder' }, 'match/from'));
+
+    const rightInput = h('.text-field',
+        h('input.form-control.relative', {
+            style: 'width:120px',
+            type: 'text',
+            value: '',
+            disabled: false,
+            id: rightInputId,
+            required: true,
+        }),
+        h('span.placeholder', { id: 'right-filter-placeholder' }, 'to/exclude'));
+
     return h('.font-size-small', [
         h('.flex-wrap.justify-between.items-center',
             h('.flex-wrap.justify-between.items-center',
@@ -73,49 +122,10 @@ export default function filteringPanel(model) {
                 }, 'Use defined filters'))),
         h('.flex-wrap.justify-between.items-center',
             h('.flex-wrap.justify-between.items-center',
-                h('select.select', {
-                    id: 'show-options-field',
-                    name: 'showOptions' }, [
-                    h('option', {
-                        value: '',
-                        selected: true,
-                        disabled: true,
-                        hidden: true,
-                    }, 'Field'),
-                    fields.map((field) => h('option', { value: field.name }, field.fieldName)),
-                ], h('.close-10-primary')),
-
-                h('select.select', {
-                    id: 'filters-opts-select',
-                    name: 'showOptions',
-                    onchange: () => onFilteringTypeChange(),
-                }, [
-                    h('option', { value: '', selected: true, disabled: true, hidden: true }, 'Filtering type'),
-                    h('option', { value: aggregatedFiltersTypes }, aggregatedFiltersTypes),
-                    h('option', { value: filterTypes.between }, filterTypes.between),
-                ], h('.close-10-primary')),
-
-                h('.text-field',
-                    h('input.form-control.relative', {
-                        style: 'width:120px',
-                        type: 'text',
-                        value: document.getElementById('filters-opts-select')?.options[Selection.selectedIndex]?.value,
-                        disabled: false,
-                        id: 'left-filter-input',
-                        required: true,
-                    }),
-                    h('span.placeholder', { id: 'left-filter-placeholder' }, 'match/from')),
-
-                h('.text-field',
-                    h('input.form-control.relative', {
-                        style: 'width:120px',
-                        type: 'text',
-                        value: '',
-                        disabled: false,
-                        id: 'right-filter-input',
-                        required: true,
-                    }),
-                    h('span.placeholder', { id: 'right-filter-placeholder' }, 'to/exclude')),
+                fieldSelect,
+                typeSelect,
+                leftInput,
+                rightInput,
 
                 h('button.btn.btn-primary', {
                     onclick: () => onFilterSubmit(),
